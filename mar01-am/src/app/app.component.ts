b26a2c5f83dbaf8a7b9678d2edb40552b764e1f4
Chuk Lee <chukmunnlee@gmail.com>
2021-03-01 11:54:36 +0800

    semi pwa

--- mar01-am/src/app/app.component.ts
+++ mar01-am/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {HttpClient, HttpParams} from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {ImageOfTheDay} from './models';
 
 // https://api.nasa.gov/planetary/apod?api_key=api_key>
@@ -11,11 +11,17 @@ const URL = 'https://api.nasa.gov/planetary/apod'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
 	iotd: ImageOfTheDay
+	canShare = false
 
 	constructor(private http: HttpClient) { }
+
+	ngOnInit() {
+		// coerce the truthy/falsey value to boolean
+		this.canShare = !!navigator['share']
+	}
 
 	async getApiKey(key: string) {
 		console.info('> api key: ', key)
@@ -24,6 +30,20 @@ export class AppComponent {
 				.set('api_key', key)
 
 		this.iotd = await this.http.get<ImageOfTheDay>(URL, { params }).toPromise()
-
 	}
+
+	async shareIt() {
+
+		if (!this.iotd)
+			return
+
+		const shareData = {
+			title: this.iotd.title,
+			text: this.iotd.explanation,
+			url: this.iotd.url
+		}
+
+		await navigator.share(shareData)
+	}
+
 }
